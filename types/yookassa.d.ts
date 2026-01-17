bab52be0f6d9b31ce627914bd2ae3a8b8cf99ca3
Natalia <nataly.tyler@gmail.com>
2026-01-17 23:36:46 +0300

    3

--- types/yookassa.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'yookassa-ts' {
-  export class YooCheckout {
-    constructor(config: { shopId: string; secretKey: string });
-    createPayment(data: any, idempotenceKey?: string): Promise<any>;
-    // можно добавить другие методы по мере необходимости
-  }
-}
